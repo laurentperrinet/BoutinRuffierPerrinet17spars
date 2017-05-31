fda78710ca9c649c21fe1535549f8eb8973fff6d
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-05-31 13:09:57 +0200

    updates

--- probe/DNN/network.py
+++ probe/DNN/network.py
@@ -42,7 +42,7 @@ class Network(object):
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
+            test_data=None, verbose=False):
         """Train the neural network using mini-batch stochastic
         gradient descent.  The ``training_data`` is a list of tuples
         ``(x, y)`` representing the training inputs and the desired
@@ -61,10 +61,11 @@ class Network(object):
             mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
-            else:
-                print("Epoch {0} complete".format(j))
+            if verbose:
+                if test_data:
+                    print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
+                else:
+                    print("Epoch {0} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
